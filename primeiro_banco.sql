c34792e9de8e087c7d879c95100e53aa048e56bd
LUIS FELIPE DA SILVA ALMEIDA <luis.fsalmeida2@senacsp.edu.br>
2026-09-02 15:35:25 -0300

    explicação dos codigos sql

--- primeiro_banco.sql
+++ primeiro_banco.sql
@@ -40,3 +40,35 @@ SELECT * FROM escola.aluno;
 SELECT nome_aluno,cidade from aluno;
 SELECT * from aluno where idade >=17;
 SELECT * from aluno where cidade = 'São paulo';
+
+--multiplicando o valor do produto
+SELECT produto, preco_unidade *12 from produto;
+
+--criando uma table para o resultado(quanto vale a duzia), mas nao "CRIA" de verdade
+select produto, preco_unidade *12 as duzia from produtos;
+
+--Nao entendi o que faz
+select produto, "Esse valor é literal" valor, preco_unidade from produtos ;
+
+--pode fazer contas com a linguagem sql
+select 0.000001* 000.9090979895 as resultado;
+
+--ordenando me ordem alfabetica
+select produto from produtos order by produto;
+--em ordem alfabetica de tras para frente
+select produto from produtos order by produto desc;
+
+--Nao seleciona apenas letras mas numeros tambem, do mais parado para o mais caro
+select produto, preco_unidade from produtos order by preco_unidade;
+
+--coloca um limite (os 10 primeiros clientes serão selecionados)
+select id,nome,email from clientes limit 10;
+
+--coloca um limite(os 10 ultimos clientes serão selecipnados)
+select id,nome,email from clientes order by id desc limit 10;
+
+--ele ira trazer 5 clientes a partir do id 6
+select id,nome from clientes limit 5 offset 5;
+
+--buscar o produto mais caro seguindo o preco como parametro, colocamos todos os produtos em ordem decrescente e limitamos para aparecer somente 1
+-select * from produtos order by preco_unidade desc limit 1; 
